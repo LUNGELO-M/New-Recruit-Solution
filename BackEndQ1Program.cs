fc8773d24f74736ce77e8024222d30d986edff1a
Lungelo <70639961+LUNGELO-M@users.noreply.github.com>
2020-09-08 09:24:15 +0200

    Back-End Question 1

---
+++ BackEndQ1Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace BackEndQ1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string theInput = "Hello ";
+            // test
+            stackOverFlow(theInput);
+        }
+
+        // the method
+        static void stackOverFlow(string theInput)
+        {
+            int theLength = theInput.Length;
+
+            Boolean A = theLength % 2 == 0;
+            Boolean B = theLength % 4 == 0;
+            string message = "No result :-(";
+            /* all multiples of 4 are multiples of 2 */
+            if (B)
+                message = "Stack Overflow";
+            else if (A)
+                message = "Stack";
+
+            Console.WriteLine(message);
+        } 
+    }
+}
