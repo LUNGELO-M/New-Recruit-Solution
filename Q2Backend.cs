38243d6ba41d15d89f6dbc0bc4e92cf683b17afc
Lungelo <70639961+LUNGELO-M@users.noreply.github.com>
2020-09-08 09:36:32 +0200

    Back-End Q2

---
+++ Q2Backend.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BackEndQ1
+{
+    public class Animal
+    {
+        public string Eat()
+        {
+            return "Yummy";
+        }
+        public virtual string MakeNoise()
+        {
+            return "Durrr";
+        }
+    }
+
+    class Horse : Animal
+    {
+        public override string MakeNoise()
+        {
+            return "Neigh";
+        }
+    }
+
+    class Sheep : Animal
+    {
+        public override string MakeNoise()
+        {
+            return "Baaah";
+        }
+    }
+}
